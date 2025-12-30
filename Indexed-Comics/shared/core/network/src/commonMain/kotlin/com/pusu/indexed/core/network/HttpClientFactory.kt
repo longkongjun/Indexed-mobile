ed5ad5c0377a726336fd7b7c9a3dbc7080f803c8
pusudewo <longkongjun@163.com>
2025-12-30 11:55:25 +0800

    DEL:old project

--- Indexed-Comics/shared/core/network/src/commonMain/kotlin/com/pusu/indexed/core/network/HttpClientFactory.kt
+++
@@ -1,7 +0,0 @@
-package com.pusu.indexed.core.network
-
-import io.ktor.client.HttpClient
-
-expect class HttpClientFactory() {
-    fun create(): HttpClient
-}
--- Indexed-Comics/shared/core/network/src/commonMain/kotlin/com/pusu/indexed/core/network/NetworkClient.kt
+++
@@ -1,7 +0,0 @@
-package com.pusu.indexed.core.network
-
-import io.ktor.client.HttpClient
-
-object NetworkClient {
-    val httpClient: HttpClient by lazy { HttpClientFactory().create() }
-}
